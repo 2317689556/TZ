ff5f83570ef91a653f27ce9823bc9e2524395dce
yhy0205 <359005136@qq.com>
2020-11-25 09:41:27 +0800

    yhy

--- src/main/java/spring/controller/UserController.java
+++ src/main/java/spring/controller/UserController.java
@@ -22,6 +22,7 @@ public class UserController {
         User user1 = userService.login(user);
         if (user1 != null) {
             session.setAttribute("USER", user1);
+            System.out.println(1);
             return "index";
         } else {
             return "login";
