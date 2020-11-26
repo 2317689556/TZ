009bc88cd8befa73c08b8c9aef15752737997e26
zhangfeng789 <1421231351@qq.com>
2020-11-26 18:33:56 +0800

    流程（张凤）

--- src/main/java/spring/service/ZFService.java
+++ src/main/java/spring/service/ZFService.java
@@ -2,11 +2,21 @@ package spring.service;
 
 import org.springframework.stereotype.Service;
 import spring.dao.ZFMapper;
+import spring.pojo.PbObjectInfo;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class ZFService {
     @Resource
     private ZFMapper zfMapper;
+
+    public List<PbObjectInfo> findInfo(){
+        return zfMapper.findInfo();
+    }
+
+    public List<PbObjectInfo> findInfoOrder() {
+        return zfMapper.findInfoOrder();
+    }
 }
