77991882a5917fd4862a0a93b959c7e3f975c462
jiale hao <2317689556@qq.com>
2020-11-27 15:55:08 +0800

    整合于浩洋（郝家乐）

---
+++ src/main/java/spring/pojo/Yhy.java
@@ -0,0 +1,24 @@
+package spring.pojo;
+
+import lombok.Data;
+
+@Data
+public class Yhy {
+    private Integer seqId;
+    private String objectId;
+    private String applyId;
+    private String fileImgUrl;
+    private String freeBidLen;
+    private String delayBidLen;
+    private String priorityEnterName;
+    private String quoteIncrement;
+    private String cmcostAmount;
+    private String trad;
+    private String bidStartTime;
+    private String bidStatus;
+    private String startPrice;
+    private String closePrice;
+    private String upRate;
+
+
+}
--- src/main/java/spring/service/YHYService.java
+++ src/main/java/spring/service/YHYService.java
@@ -2,12 +2,18 @@ package spring.service;
 
 import org.springframework.stereotype.Service;
 import spring.dao.YHYMapper;
+import spring.pojo.Yhy;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class YHYService {
 
     @Resource
     private YHYMapper yhyMapper;
+
+    public List<Yhy> findById(Integer seqId){
+        return yhyMapper.findById(seqId);
+    }
 }
